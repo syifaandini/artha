9d2648a4f02a2f80e254f2b71cdb6142206d5fef
syifaandini <syifandin@gmail.com>
2020-02-13 09:05:49 +0700

    Update app.js

--- app.js
+++ app.js
@@ -1,3 +1,35 @@
+// lOADER
+$(document).ready(function() {
+  var counter = 0;
+
+  // Start the changing images
+  setInterval(function() {
+    if(counter == 9) {
+      counter = 0;
+    }
+    counter++;
+  }, 3000);
+
+  // Set the percentage off
+  loading();
+});
+
+function loading(){
+  var num = 0;
+
+  for(i=0; i<=100; i++) {
+    setTimeout(function() {
+      $('.loader span').html(num+'%');
+
+      if(num == 100) {
+        loading();
+      }
+      num++;
+    },i*120);
+  };
+
+}
+
 // NAVBAR SCROLL HIDE-SHOW
 var prevScrollpos = window.pageYOffset;
 window.onscroll = function() {
