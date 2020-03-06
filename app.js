8ec2099b3c0ca498aed0ff8939efe475931262e0
syifaandini <syifandin@gmail.com>
2020-03-06 08:59:43 +0700

    Update app.js

--- app.js
+++ app.js
@@ -1,9 +1,9 @@
 // lOADER
+
 $('html, body').css({
   overflow: 'hidden',
   height: '100%'
 });
-
 
 $(window).on('load', function() {
 
@@ -13,8 +13,17 @@ $(window).on('load', function() {
   })
 
   $("#loading").addClass("loader-hidden");
+
+  // LOGOGERAK
+  var link = $(this).attr('href');
+
+  $('#gerak').load(link+' #content', function(){
+    $('#gerak').fadeIn(2000);
+  });
 });
 
+$('#gerak').fadeOut('slow', function(){
+  });
 
 // NAVBAR SCROLL HIDE-SHOW
 var prevScrollpos = window.pageYOffset;
@@ -32,8 +41,9 @@ window.onscroll = function() {
   prevScrollpos = currentScrollPos;
 }
 
-// PROJECT - GIFs
+
 $(document).ready(function() {
+  // PROJECT - GIFs
   $("#project1-all").hover(
     function() {
       $("#project1-img").attr("src", "images/project/floor.gif");
@@ -87,4 +97,17 @@ $(document).ready(function() {
       $("#project6-img").attr("src", "images/project/tni.png");
     }
   );
+
+  // lOGO
+  $('#title').mousemove(function (e) {
+        //parallax(e, this, 1);
+        parallax(e, document.getElementById('logoku'), 0.25);
+    });
 });
+
+function parallax(e, target, layer) {
+    var layer_coeff = 10 / layer;
+    var x = ($(window).width() - target.offsetWidth-40) / 2 - (e.pageX - ($(window).width() / 2)) / layer_coeff;
+    var y = ($(window).height() - target.offsetHeight-30) / 2 - (e.pageY - ($(window).height() / 2)) / layer_coeff;
+    $(target).offset({ top: y ,left : x });
+};
